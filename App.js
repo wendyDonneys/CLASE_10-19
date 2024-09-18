5931fc254487c3e8ec3592206dd99262ead3cdde
wendyDonneys <wendydonneys@gmail.com>
2024-09-18 07:23:56 -0500

    camvbios
    creamos la función botonclik, y ponemos una constante para que el boton desaparezca cuando hacemos clik sobre el.

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function BotonClick(){
+    const manejarClick = () => {
+    };
+    return (
+        <div>
+            <button onClick= {manejarClick}>haz click en el boton</button>
+        </div>
+    )
+}
+export default BotonClick; 
+
